3482ec1ecdc0389fcd667477de775a18cd5cbb3b
MennahAshraf <92106975+MennahAshraf@users.noreply.github.com>
2022-03-15 17:48:01 +0200

    Update deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,3 @@
 
 cd www && eb init --region us-east-1 --platform node.js udagram-api &&eb deploy Udagramapi-env && eb setenv POSTGRES_USERNAME=$username POSTGRES_PASSWORD=$password dbport=$dbport POSTGRES_HOST=$host JWT_SECRET=$jwt
 
-# database-2.cre7aro3ifmw.us-east-1.rds.amazonaws.com
