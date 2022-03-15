e210803c15a292e300d65ef5efd3125c17cd6063
MennahAshraf <92106975+MennahAshraf@users.noreply.github.com>
2022-03-15 13:28:03 +0200

    Delete deploy.sh

--- udagram-api/bin/deploy.sh
+++
@@ -1,3 +0,0 @@
-
-cd www && eb init --region us-east-1 --platform node.js udagram-api &&eb deploy Udagramapi-env && eb setenv POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres dbport=5432 POSTGRES_HOST=database-2.cre7aro3ifmw.us-east-1.rds.amazonaws.com JWT_SECRET=user_jwt
-
